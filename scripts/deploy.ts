eef1fbc7911ebf3a5d592b756351027e95df2227
patrick piemonte <patrick.piemonte@gmail.com>
2023-05-03 14:10:39 -0700

    scripts/deploy.ts, add more logging

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  const balance = await ethers.utils.formatEther(await deployer.getBalance());
+  console.log("Deploying with the account:", deployer.address);
+  console.log("Account balance:", balance);
+
   const Decoder = await ethers.getContractFactory("PixelChainDecoder");
   const decoder = await Decoder.deploy();
 
